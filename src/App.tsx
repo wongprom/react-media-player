c3746c278e3886b3fc832830ba53f0c66d6c7e85
wongprom <>
2024-07-29 14:44:08 +0200

    Create simpel header with icons and text.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import Header from './components/Header';
+
 export function App() {
   return (
     <>
-      <h1>This is the App component</h1>
+      <Header />
     </>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import './Header.css';
+
+const Header = () => {
+  return (
+    <header>
+      <span className="material-symbols-outlined">chevron_left</span>
+      <p>Playlist - Moody</p>
+      <span className="material-symbols-outlined">more_vert</span>
+    </header>
+  );
+};
+
+export default Header;
