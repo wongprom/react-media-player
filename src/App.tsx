6b833d84388a8bfe9d4fe3940ab6792093a1bef1
wongprom <>
2024-07-29 15:20:10 +0200

    Create two columns with grid-template-columns songs nad player.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import Header from './components/Header';
+import './App.css';
 
 export function App() {
   return (
     <>
       <Header />
+      <main>
+        <section className="song-list"></section>
+        <section className="music-player"></section>
+      </main>
     </>
   );
 }
