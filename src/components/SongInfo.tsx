2185537f2da33a5f5fbb7541f0638736b92ef469
wongprom <>
2024-07-30 17:05:23 +0200

    Style ActiveSong and SongInfo components.

--- src/components/SongInfo.tsx
+++ src/components/SongInfo.tsx
@@ -9,14 +9,16 @@ export const SongInfo = ({ artist, title }: ISongInfoProps): ReactElement => {
   return (
     <div className="active-song-info">
       <div>
-        <span className="material-symbols-outlined">more_vert</span>
+        <span className="material-symbols-outlined font-size-30">
+          add_circle
+        </span>
+      </div>
+      <div className="active-album-info">
+        <p className="artist">{artist}</p>
+        <p className="title">{title}</p>
       </div>
       <div>
-        <p>{artist}</p>
-        <p>{title}</p>
-      </div>
-      <div>
-        <span className="material-symbols-outlined">more_vert</span>
+        <span className="material-symbols-outlined font-size-30">favorite</span>
       </div>
     </div>
   );
