50787628c72f432d8321cf8b66ecdb284ea15a75
wongprom <>
2024-07-30 17:48:05 +0200

    Style Header fonts.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,9 +4,11 @@ import './Header.css';
 export const Header = (): ReactElement => {
   return (
     <header>
-      <span className="material-symbols-outlined">chevron_left</span>
+      <span className="material-symbols-outlined font-size-30">
+        chevron_left
+      </span>
       <p>Playlist - Moody</p>
-      <span className="material-symbols-outlined">more_vert</span>
+      <span className="material-symbols-outlined font-size-30">more_vert</span>
     </header>
   );
 };
