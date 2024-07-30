81d5fbbe70120c4e5f087dcc84d215386dc9cf1a
wongprom <>
2024-07-30 17:43:26 +0200

    Style Controls and some whitespace to ActiveSong

--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -4,15 +4,21 @@ export const Controls = () => {
   return (
     <div className="controls">
       <div>
-        <span className="material-symbols-outlined">repeat</span>
+        <span className="material-symbols-outlined font-size-30">repeat</span>
       </div>
       <div className="center-controls">
-        <span className="material-symbols-outlined">fast_rewind</span>
-        <span className="material-symbols-outlined">play_circle</span>
-        <span className="material-symbols-outlined">fast_forward</span>
+        <span className="material-symbols-outlined font-size-50">
+          fast_rewind
+        </span>
+        <span className="material-symbols-outlined play_circle font-size-70">
+          play_circle
+        </span>
+        <span className="material-symbols-outlined font-size-50">
+          fast_forward
+        </span>
       </div>
       <div>
-        <span className="material-symbols-outlined">shuffle</span>
+        <span className="material-symbols-outlined font-size-30">shuffle</span>
       </div>
     </div>
   );
